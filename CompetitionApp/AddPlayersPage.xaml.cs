2c298c96c1e6c80e2c5820a472f5318ffebeb2b3
Paweł Sitarz <pawel.sitarz98@gmail.com>
2020-12-10 23:38:11 +0100

    Complete the AddPlayersPage at frontend.

--- CompetitionApp/AddPlayersPage.xaml.cs
+++ CompetitionApp/AddPlayersPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,42 @@ namespace CompetitionApp
         public AddPlayers()
         {
             InitializeComponent();
+        }
+
+        private void BtnAddPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection(@"Data Source=PAWEŁ-KOMPUTER\SQLEXPRESS; Initial Catalog=RegistredUsers; Integrated Security=True"); //connection data
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed)
+                    sqlConnection.Open();
+                var query = "SELECT COUNT(1) FROM tblClub WHERE Login=@Login AND Password=@Password";   //sql command
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddWithValue("@Login", TxtName.Text);
+                sqlCommand.Parameters.AddWithValue("@Password", TxtSurname.Text);
+                int count = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                if (count == 1)
+                {
+
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Login or Password is incorrect");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();  //closing database connection
+            }
         }
     }
 }
